fca4f853ee8b10e3d0a80c420909d9f5092dc151
yleonf <yasnaleonfoitzick@gmail.com>
2025-05-12 18:14:05 -0400

    Calculo Promedio

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const students=[]
 const tableBody=document.querySelector("#studentsTable tbody");
+const averageDiv=document.getElementById("average");
 document.getElementById("studentForm").addEventListener("submit",function (e){
  e.preventDefault();
 
@@ -17,6 +18,7 @@ document.getElementById("studentForm").addEventListener("submit",function (e){
  students.push(student);
  //console.log(students)
 addStudentToTable(student)
+calcularPromedio();
  this.reset()
 
 });
@@ -29,3 +31,14 @@ function addStudentToTable(student){
         <td>${student.grade}</td>`;
      tableBody.appendChild(row);   
        }
+
+       function calcularPromedio(){
+           if(students.length===0){
+             averageDiv.textContent="Promedio General del Curso : N/A"
+             return
+           }
+            const total=students.reduce((sum,student)=>sum+student.grade,0);
+           const prom=total/students.length;
+           averageDiv.textContent="Promedio General del Curso : "+prom.toFixed(2);
+       }
+
